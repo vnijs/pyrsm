ead81165e5d82f99f58f2399351f326cfa5c9e08
vnijs <vnijs@ucsd.edu>
2021-02-24 23:40:46 -0800

    0.4.5

--- pyrsm/notebook.py
+++ pyrsm/notebook.py
@@ -2,7 +2,23 @@ import pickle
 import ipynbname
 
 
-def save_state(path=None):
+def save_state(dct, path=None):
+    """
+    Store the (partial) state of a Jupyter notebook
+
+    Parameters
+    ----------
+    dct : dict
+        Dictionary of python objects to store (e.g., globals())
+    path : str
+        File path to use to store state. If none, (try) to use the notebook name
+        If the notebook name cannot be determined, uses 'notebook.state.pkl'
+
+    Examples
+    --------
+    save_state(globals(), path="my-notebook.state.pkl")
+    """
+
     remove_keys = [
         "In",
         "Out",
@@ -26,27 +42,52 @@ def save_state(path=None):
     ]
     state = {
         key: val
-        for key, val in globals().items()
+        for key, val in dct.items()
         if (
             not key.startswith("_")
             and (key not in remove_keys)
             and (str(type(val)) not in remove_types)
         )
     }
-
     if path is None:
-        path = ipynbname.name() + ".state.pkl"
+        try:
+            path = ipynbname.name() + ".state.pkl"
+        except:
+            path = "notebook.state.pkl"
 
     with open(path, "wb") as f:
         pickle.dump(state, f)
 
 
-def load_state(path=None):
+def load_state(dct=None, path=None):
+    """
+    Re-store the (partial) state of a Jupyter notebook
+
+    Parameters
+    ----------
+    dct : dict
+        Dictionary to add python objects to (e.g., globals()).
+        If None, a dictionary with objects will be returned
+    path : str
+        Path to use to store state. If none, (try) to use the current notebook name.
+        If the notebook name cannot be determined, uses 'notebook.state.pkl'
+
+    Examples
+    --------
+    load_state(globals(), path="my-notebook.state.pkl")
+    """
     if path is None:
-        path = ipynbname.name() + ".state.pkl"
+        try:
+            path = ipynbname.name() + ".state.pkl"
+        except:
+            path = "notebook.state.pkl"
 
     with open(path, "rb") as f:
         g = pickle.load(f)
 
-    for key, val in g.items():
-        globals()[key] = val
+    if dct is None:
+        return g
+    else:
+        # using the mutability feature in python
+        for key, val in g.items():
+            dct[key] = val
