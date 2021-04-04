63d2e50a39514f3cb1dd5a01c173d0adfe420f21
vnijs <vnijs@ucsd.edu>
2021-04-04 05:22:37 -0700

    expanded functionality

--- pyrsm/basics.py
+++ pyrsm/basics.py
@@ -4,10 +4,42 @@ import pandas as pd
 from scipy import stats
 import seaborn as sns
 from pyrsm.logit import sig_stars
+from pyrsm.utils import ifelse
 
 
 class cross_tabs:
     def __init__(self, df, var1, var2):
+        """
+        Calculate a Chi-square test between two categorical variables contained
+        in a Pandas dataframe
+
+        Parameters
+        ----------
+        df : Pandas dataframe with numeric variables
+
+        Returns
+        -------
+        Cross object with several attributes
+        df: Original dataframe
+        var1: Name of the first categorical variable
+        var2: Name of the second categorical variable
+        observed: Dataframe of observed frequencies
+        expected: Dataframe of expected frequencies
+        expected_low: List with number of cells with expected values < 5
+            and the total number of cells
+        chisq: Dataframe of chi-square values for each cell
+        dev_std: Dataframe of standardized deviations from the expected table
+        perc_row: Dataframe of observation percentages conditioned by row
+        perc_col: Dataframe of observation percentages conditioned by column
+        perc: Dataframe of observation percentages by the total number of observations
+
+        Examples
+        --------
+        import pyrsm as rsm
+        rsm.load_data(pkg="basics", name="newspaper", dct=globals())
+        ct = rsm.cross_tabs(newspaper, "Income", "Newspaper")
+        ct.expected
+        """
         self.df = df
         self.var1 = var1
         self.var2 = var2
@@ -15,18 +47,26 @@ class cross_tabs:
         self.observed = pd.crosstab(
             df[var1], columns=df[var2], margins=True, margins_name="Total"
         )
-        self.chisq = stats.chi2_contingency(
+        self.chisq_test = stats.chi2_contingency(
             self.observed.drop(columns="Total").drop("Total", axis=0), correction=False
         )
-        expected = pd.DataFrame(self.chisq[3])
+        expected = pd.DataFrame(self.chisq_test[3])
+        self.expected_low = [
+            (expected < 5).sum().sum(),
+            expected.shape[0] * expected.shape[0],
+        ]
         expected["Total"] = expected.sum(axis=1)
         expected = expected.append(expected.sum(), ignore_index=True).set_index(
             self.observed.index
         )
         expected.columns = self.observed.columns
         self.expected = expected
-        self.chi_sq = (
-            ((self.observed - self.expected) ** 2 / self.expected)
+        chisq = (self.observed - self.expected) ** 2 / self.expected
+        chisq["Total"] = chisq.sum(axis=1)
+        chisq.loc["Total", :] = chisq.sum()
+        self.chisq = chisq
+        self.dev_std = (
+            ((self.observed - self.expected) / np.sqrt(self.expected))
             .drop(columns="Total")
             .drop("Total", axis=0)
         )
@@ -35,6 +75,30 @@ class cross_tabs:
         self.perc = self.observed / self.observed.loc["Total", "Total"]
 
     def summary(self, output=["observed", "expected"], dec=2):
+        """
+        Print different output tables for a cross_tabs object
+
+        Parameters
+        ----------
+        output : list of tables to show
+            Options include "observed" (observed frequencies),
+            "expected" (expected frequencies), "chisq" (chi-square values)
+            for each cell, "dev_std" (standardized deviations from expected)
+            "perc_row" (percentages conditioned by row), "perc_col"
+            (percentages conditioned by column), "perc" (percentages by the
+            total number of observations). The default value is ["observed", "expected"]
+        dec : int
+            Number of decimal places to use in rounding
+
+        Examples
+        --------
+        import pyrsm as rsm
+        rsm.load_data(pkg="basics", name="newspaper", dct=globals())
+        ct = rsm.cross_tabs(newspaper)
+        ct.summary()
+        """
+
+        output = ifelse(type(output) is list, output, [output])
         prn = f"""
 Cross-tabs
 Variables: {self.var1}, {self.var2}
@@ -42,73 +106,132 @@ Null hyp: there is no association between {self.var1} and {self.var2}
 Alt. hyp: there is an association between {self.var1} and {self.var2}
 """
         if "observed" in output:
-            prn = (
-                prn
-                + f"""
+            prn += f"""
 Observed:
 
 {self.observed.applymap(lambda x: "{:,}".format(x))}
 """
-            )
-        if "observed" in output:
-            prn = (
-                prn
-                + f"""
+        if "expected" in output:
+            prn += f"""
 Expected: (row total x column total) / total
 
 {self.expected.round(dec).applymap(lambda x: "{:,}".format(x))}
 """
-            )
         if "chisq" in output:
-            prn = (
-                prn
-                + f"""
+            prn += f"""
 Contribution to chi-squared: (o - e)^2 / e
 
-{self.chi_sq.round(dec).applymap(lambda x: "{:,}".format(x))}
+{self.chisq.round(dec).applymap(lambda x: "{:,}".format(x))}
 """
-            )
+
+        if "dev_std" in output:
+            prn += f"""
+Deviation standardized: (o - e) / sqrt(e)
+
+{self.dev_std.round(dec).applymap(lambda x: "{:,}".format(x))}
+"""
+
         if "perc_row" in output:
-            prn = (
-                prn
-                + f"""
+            prn += f"""
 Row percentages:
 
 {self.perc_row.transform(lambda x: (100*x).round(dec).astype(str) + "%")}
 """
-            )
         if "perc_col" in output:
-            prn = (
-                prn
-                + f"""
+            prn += f"""
 Column percentages:
 
 {self.perc_col.transform(lambda x: (100*x).round(dec).astype(str) + "%")}
 """
-            )
-
         if "perc_all" in output:
-            prn = (
-                prn
-                + f"""
+            prn += f"""
 Percentages:
 
 {self.perc.transform(lambda x: (100*x).round(dec).astype(str) + "%")}
 """
-            )
-
-        prn = (
-            prn
-            + f"""
-Chi-squared: {round(self.chisq[0], dec)} df({round(self.chisq[2], dec)}), p.value {round(self.chisq[1], dec)}
+        prn += f"""
+Chi-squared: {round(self.chisq_test[0], dec)} df({int(self.chisq_test[2])}), p.value {ifelse(self.chisq_test[1] < 0.001, "< .001", round(self.chisq_test[1], dec))}
+{100 * round(self.expected_low[0] / self.expected_low[1], dec)}% of cells have expected values below 5
 """
-        )
         print(prn)
 
-    # Another instance method
     def plot(self, output="perc_col", **kwargs):
-        pdf = getattr(self, output).drop(columns="Total").drop("Total", axis=0)
-        fig = pdf.plot.bar(**kwargs)
+        """
+        Plot of correlations between numeric variables in a Pandas dataframe
+
+        Parameters
+        ----------
+        output : list of tables to show
+            Options include "observed" (observed frequencies),
+            "expected" (expected frequencies), "chisq" (chi-square values)
+            for each cell, "dev_std" (standardized deviations from expected)
+            "perc_row" (percentages conditioned by row), "perc_col"
+            (percentages conditioned by column), "perc" (percentages by the
+            total number of observations). The default value is ["observed", "expected"]
+        **kwargs : Named arguments to be passed to pandas plotting functions
+
+        Examples
+        --------
+        import pyrsm as rsm
+        rsm.load_data(pkg="basics", name="newspaper", dct=globals())
+        ct = rsm.cross_tabs(newspaper, "Income", "Newspaper")
+        ct.plot()
+        """
+        output = ifelse(type(output) is list, output, [output])
+
+        args = {"rot": False}
+        if "observed" in output:
+            df = (
+                self.observed.transpose()
+                .drop(columns="Total")
+                .drop("Total", axis=0)
+                .apply(lambda x: x * 100 / sum(x), axis=1)
+            )
+            args["title"] = "Observed frequencies"
+            args.update(**kwargs)
+            fig = df.plot.bar(stacked=True, **args)
+        if "expected" in output:
+            df = (
+                self.expected.transpose()
+                .drop(columns="Total")
+                .drop("Total", axis=0)
+                .apply(lambda x: x * 100 / sum(x), axis=1)
+            )
+            args["title"] = "Expected frequencies"
+            args.update(**kwargs)
+            fig = df.plot.bar(stacked=True, **args)
+        if "chisq" in output:
+            df = self.chisq.transpose().drop(columns="Total").drop("Total", axis=0)
+            args["title"] = "Contribution to chi-squared statistic"
+            args.update(**kwargs)
+            fig = df.plot.bar(**args)
+        if "dev_std" in output:
+            df = self.dev_std.transpose()
+            args["title"] = "Deviation standardized"
+            args.update(**kwargs)
+            fig, ax = plt.subplots()
+            df.plot.bar(**args, ax=ax)
+            ax.axhline(y=1.96, color="black", linestyle="--")
+            ax.axhline(y=1.64, color="black", linestyle="--")
+            ax.axhline(y=-1.96, color="black", linestyle="--")
+            ax.axhline(y=-1.64, color="black", linestyle="--")
+            ax.annotate("95%", xy=(0, 2.1), va="bottom", ha="center")
+            ax.annotate("90%", xy=(0, 1.4), va="top", ha="center")
+        if "perc_col" in output:
+            df = self.perc_col.transpose().drop(columns="Total").drop("Total", axis=0)
+            args["title"] = "Column percentages"
+            args.update(**kwargs)
+            fig = df.plot.bar(**args)
+        if "perc_row" in output:
+            df = self.perc_row.transpose().drop(columns="Total").drop("Total", axis=0)
+            args["title"] = "Row percentages"
+            args.update(**kwargs)
+            fig = df.plot.bar(**args)
+        if "perc" in output:
+            df = self.perc.transpose().drop(columns="Total").drop("Total", axis=0)
+            args["title"] = "Table percentages"
+            args.update(**kwargs)
+            fig = df.plot.bar(**args)
 
 
 class correlation:
@@ -128,8 +251,10 @@ class correlation:
 
         Examples
         --------
-        df = pd.DataFrame({"x": [0, 1, 1, 1, 0], "y": [1, 0, 0, 0, np.NaN]})
-        c = correlation(df)
+        import pandas as pd
+        import pyrsm as rsm
+        rsm.load_data(pkg="basics", name="salary", dct=globals())
+        cr = rsm.correlation(salary[["salary", "yrs.since.phd", "yrs.service"]])
         c.cr
         """
         df = df.copy()
@@ -154,19 +279,24 @@ class correlation:
         self.cr = cr
         self.cp = cp
 
-    def summary(self, dec=2):
+    def summary(self, cutoff=0, dec=2):
         """
         Print correlations between numeric variables in a Pandas dataframe
 
         Parameters
         ----------
+        cutoff : float
+            Only show correlations larger than a threshold in absolute value
         dec : int
             Number of decimal places to use in rounding
 
         Examples
         --------
-        df = pd.DataFrame({"x": [0, 1, 1, 1, 0], "y": [1, 0, 0, 0, np.NaN]})
-        correlation(df).summary()
+        import pandas as pd
+        import pyrsm as rsm
+        rsm.load_data(pkg="basics", name="salary", dct=globals())
+        cr = rsm.correlation(salary[["salary", "yrs.since.phd", "yrs.service"]])
+        cr.summary()
         """
         ind = np.triu_indices(self.cr.shape[0])
         cn = self.df.columns[:-1]
@@ -174,6 +304,8 @@ class correlation:
 
         # correlations
         crs = self.cr.round(dec).astype(str)
+        if cutoff > 0:
+            crs[np.abs(self.cr) < cutoff] = ""
         crs[ind] = ""
         crs = pd.DataFrame(
             np.delete(np.delete(crs, 0, axis=0), crs.shape[1] - 1, axis=1),
@@ -183,6 +315,8 @@ class correlation:
 
         # pvalues
         cps = self.cp.round(dec).astype(str)
+        if cutoff > 0:
+            cps[np.abs(self.cr) < cutoff] = ""
         cps[ind] = ""
         cps = pd.DataFrame(
             np.delete(np.delete(cps, 0, axis=0), cps.shape[1] - 1, axis=1),
@@ -198,11 +332,11 @@ class correlation:
             x = cn[0]
             y = cn[1]
 
-        s = "Correlation\n"
-        s += "Variables: " + ", ".join(list(self.df.columns)) + "\n"
-        s += "Null hyp.: " + f"variables {x} and {y} are not correlated\n"
-        s += "Alt. hyp.: " + f"variables {x} and {y} are correlated\n"
-        print(s)
+        prn = "Correlation\n"
+        prn += "Variables: " + ", ".join(list(self.df.columns)) + "\n"
+        prn += f"Null hyp.: variables {x} and {y} are not correlated\n"
+        prn += f"Alt. hyp.: variables {x} and {y} are correlated\n"
+        print(prn)
         print("Correlation matrix:")
         print(crs)
         print("\np.values:")
@@ -215,7 +349,8 @@ class correlation:
         Parameters
         ----------
         nobs : int
-            Number of observations to use for the scatter plots
+            Number of observations to use for the scatter plots. The default
+            value is 1,000. To use all observations in the plots, use nobs=-1
         dec : int
             Number of decimal places to use in rounding
         figsize : tuple
@@ -225,8 +360,11 @@ class correlation:
 
         Examples
         --------
-        df = pd.DataFrame({"x": [0, 1, 1, 1, 0], "y": [1, 0, 0, 0, np.NaN]})
-        correlation(df).plot()
+        import pandas as pd
+        import pyrsm as rsm
+        rsm.load_data(pkg="basics", name="salary", dct=globals())
+        cr = rsm.correlation(salary[["salary", "yrs.since.phd", "yrs.service"]])
+        cr.plot(figsize=(7, 3))
         """
 
         def cor_label(label, longest, ax_sub):
