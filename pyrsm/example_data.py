c8c53c1932a55d3641b4ade0f851205a9dd0cfef
vnijs <vnijs@ucsd.edu>
2023-07-03 16:38:47 -0700

    update to use parquet

--- pyrsm/example_data.py
+++ pyrsm/example_data.py
@@ -18,44 +18,60 @@ def load_data(pkg=None, name=None, dct=None):
         If None, all datasets will be returned
     dct : dct
         Dictionary to add datasets to. For example, using globals() will add
-        dataframes to the global environment in python. If None, a dictioary
+        dataframes to the global environment in python. If None, a dictionary
         with datasets will be returned
 
     Examples
     --------
-    load_data(globals(), path="my-notebook.state.pkl")
+
+    import pyrsm as rsm
+    rsm.load_data(pkg="basics", name="demand_uk", dct=globals())
     """
 
     base = "pyrsm.data"
     base_path = import_module(base).__path__[0]
 
-    def load(file_path):
-        return pd.read_pickle(file_path)
+    def load_data(file_path):
+        return pd.read_parquet(file_path)
+
+    def load_description(file_path):
+        # read a text file into a string
+        with open(f"{file_path}_description.md", "r") as file:
+            descr = file.read()
+        return descr
 
     def mkdct(spkg):
         data = {}
+        description = {}
         for sp in spkg:
             package_path = os.path.join(base_path, sp)
             for file_name in os.listdir(package_path):
                 if (name is None and not file_name.startswith(("__", "."))) or (
-                    name is not None and file_name == f"{name}.pkl"
+                    name is not None and file_name == f"{name}.parquet"
                 ):
                     file_path = os.path.join(package_path, file_name)
-                    key = file_name.replace(".pkl", "")
-                    data[key] = load(file_path)
-        return data
+                    key = file_name.replace(".parquet", "")
+                    data[key] = load_data(file_path)
+                    description[key] = load_description(file_path)
+        return data, description
 
     if pkg is None:
-        data = mkdct([d for d in os.listdir(base_path) if not d.startswith("__")])
+        data, description = mkdct(
+            [d for d in os.listdir(base_path) if not d.startswith("__")]
+        )
     elif name is None:
-        data = mkdct([pkg])
+        data, description = mkdct([pkg])
     elif pkg is not None and name is not None:
-        data = {name: load(os.path.join(base_path, pkg, f"{name}.pkl"))}
+        data = load_data(os.path.join(base_path, pkg, f"{name}.parquet"))
+        description = load_description(os.path.join(base_path, pkg, name))
     else:
         data = {}
+        description = {}
 
     if dct is None:
-        return data
+        return data, description
     else:
         for key, val in data.items():
             dct[key] = val
+        for key, val in description.items():
+            dct[f"{key}_description"] = val
